469a3212ce4dec417832db35442a70db93967ad6
Josh Agron <jagron907@gmail.com>
2021-06-23 16:51:48 -0800

    Can now save output as a multiline adjacency list

--- grapher.py
+++ grapher.py
@@ -15,7 +15,11 @@ def usage(exitVal, filename=""):
     -a  NAME        Append NAME to the output file (default: -output)
     -s              Use sum of weights as weight for edge (default: max weight per argument)
     -j              Save output as a .json file (default: .txt file)
-    -g              Output the final graph (default: only outputs parsed dictionary)
+    -p              Output the parsed dictionary (default: outputs the adjacency list)
+    -g              Output a human-readable graph (default: outputs the adjacency list)
+    -adj            Output an undirected, unweighted multiline adjacency list
+    -ud             Output a undirected multiline adjacency list (NOTE: only works with -adj flag)
+    -uw             Output a unweighted miltiline adjacency list (NOTE: only works with -adj flag)
     -n  NAME        Name to save the output as (default: <current file name>-output.txt)
     -D  DIR         Directory to save the output to (default: output-files/)
                     WARNING: If directory does not exist, it will create a new one with the inputted name
@@ -46,9 +50,12 @@ def main():
     output_folder   = 'output-files/'
     out_name        = False
     single_conn     = False
-    parse_only      = True
+    parse_only      = False
+    graph_only      = False
     save_as_json    = False
     use_sum         = False
+    undirected      = False
+    unweighted      = False
     append          = "-output"
     
     while arguments and arguments[0].startswith('-'):
@@ -58,8 +65,10 @@ def main():
             output_folder = arguments.pop(0) + '/'
         elif argument == '-o':
             single_conn = True
+        elif argument == '-p':
+            parse_only = True
         elif argument == '-g':
-            parse_only = False
+            graph_only = True
         elif argument == '-n':
             out_name = arguments.pop(0)
         elif argument == '-j':
@@ -68,6 +77,12 @@ def main():
             use_sum = True
         elif argument == '-a':
             append = arguments.pop(0)
+        elif argument == '-adj':
+            adj_list = True
+        elif argument == '-ud':
+            undirected = True
+        elif argument == '-uw':
+            unweighted = True
         elif argument == '-h':
             usage(0)
         else:
@@ -128,12 +143,51 @@ def main():
                 graph[call] = graph.get(call, {})
 
                 for dep in parse[phandle][argument][call].keys():
-                    if use_sum:
+                    if unweighted:
+                        graph[call][dep] = 1
+                    elif use_sum:
                         graph[call][dep] = graph[call].get(dep, 0) + parse[phandle][argument][call][dep]
                     else:
                         graph[call][dep] = max(graph[call].get(dep, 0), parse[phandle][argument][call][dep])
 
-    print_to_file(output_name, graph, save_as_json)
+    parse.clear()
+    
+    if undirected:
+        edges = []
+        for syscall1 in graph.keys():
+            for syscall2 in graph[syscall1].keys():
+                edge = {syscall1, syscall2}
+                if edge not in edges:
+                    edges.append(edge)
+                    continue
+
+                # edge between these two system calls already exists
+                if unweighted:
+                    graph[syscall1][syscall2] = 1
+                elif use_sum:
+                    graph[syscall1][syscall2] += graph[syscall2][syscall1]
+                else:
+                    graph[syscall1][syscall2] = max(graph[syscall1][syscall2], graph[syscall2][syscall1])
+                graph[syscall2].pop(syscall1)
+
+    if graph_only:
+        print_to_file(output_name, graph, save_as_json)
+
+    newline = False
+    f = open(output_name, "w")
+    for s1 in graph.keys():
+        if not graph[s1]:
+            continue
+        if newline:
+            f.write('\n')
+        newline = True
+        f.write(f'{s1} {len(graph[s1])}')
+        for s2 in graph[s1].keys():
+            f.write(f'\n{s2}')
+            if not unweighted:
+                writeLine = " {'weight':" + str(graph[s1][s2]) + "}"
+                f.write(writeLine)
+
 
 if __name__ == '__main__':
     main()
